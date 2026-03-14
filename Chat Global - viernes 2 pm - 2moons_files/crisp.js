c9ed6c056aea2a5927dd8870519730c87d130f00
R0LO <rolo08070@gmail.com>
2026-03-14 15:29:40 +0100

    subiendo

---
+++ Chat Global - viernes 2 pm - 2moons_files/crisp.js
@@ -0,0 +1,8 @@
+/*$(function (){
+window.$crisp=[];
+
+// your crisp token 
+window.CRISP_WEBSITE_ID="";
+
+(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+})*/
